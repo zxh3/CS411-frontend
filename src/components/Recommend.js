62f11741a6142b0ba53ede289b422d23b5863ddf
熊思琪 <siqixiong@wirelessprvnat-172-16-238-23.near.illinois.edu>
2018-11-28 22:46:18 -0600

    recommend

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import M from 'materialize-css';
 
 class Recommend extends Component {
+  state = {
+    email: "",
+  }
 
   handleRecommend = (e) => {
     e.preventDefault();
-    console.log(`RRRRRRRRRRRRRRRRRecommending ${this.props.dishName} to ${e}`);
-    axios.post('https://cs411-backend.herokuapp.com/recommend', {
-     user_email: e,
-      dishName: this.props.dishName
+    let email = this.state.email;
+    let dishName = this.props.dishName;
+    if (this.state.email.length === 0) {
+      M.toast({html: 'Fields cannot be empty'});
+      return;
+    } else {
+      console.log(email);
+      console.log(dishName);
+      axios.post('https://cs411-backend.herokuapp.com/recommend', {
+        email,
+        dishName
+      })
+        .then(res => {
+          if (res.data.error) {
+            // console.log(res.data.error);
+            M.toast({html: res.data.error});
+          } else {
+            M.toast({html: "Success!"});
+          }
+        })
+        .catch(err => console.error(err));
+    }
+    this.setState({
+        email: "",
     });
+    // e.preventDefault();
+    // console.log(`RRRRRRRRRRRRRRRRRecommending ${this.props.dishName} to ${e}`);
+    // axios.post('https://cs411-backend.herokuapp.com/recommend', {
+    //  user_email: e,
+    //   dishName: this.props.dishName
+    // });
   }
 
   handleChange = (e) => {
@@ -23,17 +53,17 @@ class Recommend extends Component {
         <div>
 
             <div className="recommendbtn">
-            <div className="btn-small modal-trigger" href="#modal-recommend">Recommend</div>                
+            <div className="btn-small modal-trigger" href={`#rec${this.props.dishName}`}>Recommend</div>                
             </div>
 
-            <div id="modal-recommend" className="modal">
+            <div id={`rec${this.props.dishName}`} className="modal">
                 <div className="modal-content">
                     <h3 className="red-text text-lighten-2">Recommend this dish to your friend!</h3>
 
                     <form className="input-field" onSubmit={this.handleRecommend}>
-                        <input id="user_email" type="text" onChange={this.handleChange}/>
-                        <label htmlFor="user_email">Enter your friend's email here!</label>
-                        <button className="btn">Submit</button>
+                      <input id="email" type="text" onChange={this.handleChange} value={this.state.email} autoComplete="off" />
+                      <label htmlFor="email">Enter your friend's email here!</label>
+                      <button className="btn">Submit</button>
                     </form>
                     
                 </div>
