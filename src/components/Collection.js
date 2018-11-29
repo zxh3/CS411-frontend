9f882fb3d5bfaab725f101480acbf29f07ac69d0
Xiaohua <cheonghiuwaa@gmail.com>
2018-11-29 00:58:27 -0600

    delete collection item

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -5,7 +5,7 @@ import { isEqual } from 'lodash';
 class Collection extends Component {
   state = {
     dishNames: [],
-    collectionName: ''
+    collectionName: '',
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -48,16 +48,29 @@ class Collection extends Component {
       .catch(err => console.error(err));
   }
 
+  handleDelete = (dishName, collectionid) => {
+    axios.post(`https://cs411-backend.herokuapp.com/deletecollectionitem`, {
+      dishName, collectionid
+    }).then(res => {
+      this.setState(prevState => {
+        return {
+          dishNames: prevState.dishNames.filter(x => x !== dishName)
+        }
+      }).catch(err => console.error(err));
+    })
+  }
+
   render() {
     let items = this.state.dishNames.map(dishName => (
       <li key={dishName} className="collection-item">
         {dishName}
+        <i onClick={() => this.handleDelete(dishName, this.props.collectionid)} className="material-icons right">delete_sweep</i>
       </li>
     ));
 
     return (
       <div>        
-        <ul className="col s3 collection with-header">
+        <ul className="col s5 offset-s1 collection with-header">
           <button onClick={() => this.props.handleDelete(this.props.collectionid)} className="btn-floating red right"><i className="material-icons">delete_forever</i></button>
           <li className="collection-header"><h4>{this.state.collectionName}</h4></li>
           {items}
